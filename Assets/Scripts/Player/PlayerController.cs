c7ea959d1c03668ad6a9ee543d6a6e48bc72b4e3
ViriusLevan <Mantor562@gmail.com>
2023-08-20 13:37:30 +0700

    Revert "Merge branch 'main' into Loop"
    This reverts commit 3401de2dfb67ef44dd7c2b77feb32b301383faa6, reversing
    changes made to 898a1ed1074a36d214a9b7bab854e192d2f6bdab.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -17,37 +17,24 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField] private float maxFallSpeed = 20.0f;
 	[SerializeField] private float rotateSpeed = 25f; //Speed the player rotate
 
-  private Rigidbody rb;
+    private Rigidbody rb;
 	private Vector3 moveDir;
 	private Vector3 pushDir;
-  private Vector2 moveInput;
-  private float distToGround;
+    private Vector2 moveInput;
+    private float distToGround;
 	private float pushForce;
 
-  private bool canMove = true; //If player is not hit
+    private bool canMove = true; //If player is not hit
 	private bool isStunned = false;
 	private bool wasStunned = false; //If player was stunned before get stunned another time
 	private bool slide = false;
-  private bool jumped;
-
-  public void OnJump(InputAction.CallbackContext context) => jumped = context.action.triggered;
-
-	// Double Jump PowerUp
-	private int jumpsLeft = 0; // Number of jumps left, including double jumps
-	private int maxJumps = 2; // Maximum number of jumps allowed, including double jumps
-	private bool doubleJumpAvailable = false; // Whether the double jump power-up is available
-
-	public bool hasExtraLife { get; private set; }
-	public bool hasShield { get; private set; }
-
+    private bool jumped;
+    public void OnJump(InputAction.CallbackContext context) => jumped = context.action.triggered;
 
 	private void Start ()
 	{
 		// get the distance to ground
 		distToGround = GetComponent<Collider>().bounds.extents.y;
-
-		doubleJumpAvailable = false;
-    	jumpsLeft = maxJumps;
 	}
 	
 	private bool IsGrounded ()
@@ -89,8 +76,6 @@ public class PlayerController : MonoBehaviour {
 
 			if (IsGrounded())
 			{
-				jumpsLeft = maxJumps;
-
 			 	// Calculate how fast we should be moving
 				Vector3 targetVelocity = moveDir;
 				targetVelocity *= speed;
@@ -123,15 +108,13 @@ public class PlayerController : MonoBehaviour {
 				if (IsGrounded() && jumped)
 				{
                     rb.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
-					jumpsLeft -= 1;
-					jumped = false;
 				}
 			}
 			else
 			{
 				if (!slide)
 				{
-					Vector3 targetVelocity = new Vector3(moveDir.x * airVelocity, rb.velocity.y, moveDir.z * airVelocity);
+					Vector3 targetVelocity = new Vector3(moveDir.x * airVelocity * 0.25f, rb.velocity.y, moveDir.z * airVelocity * 0.25f);
 					Vector3 velocity = rb.velocity;
 					Vector3 velocityChange = (targetVelocity - velocity);
 					velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
@@ -144,40 +127,13 @@ public class PlayerController : MonoBehaviour {
 				{
 					rb.AddForce(moveDir * 0.15f, ForceMode.VelocityChange);
 				}
-
-				// Double jump
-				if (jumpsLeft > 0 && jumped)
-				{
-					if (!IsGrounded())
-					{
-						if (doubleJumpAvailable)
-						{
-							rb.velocity = new Vector3(rb.velocity.x, CalculateJumpVerticalSpeed(), rb.velocity.z);
-							jumpsLeft -= 1;
-							doubleJumpAvailable = false; // Disable double jump after using it
-						}
-						else if (jumpsLeft > 1) // Check if regular jumps are still available
-						{
-							rb.velocity = new Vector3(rb.velocity.x, CalculateJumpVerticalSpeed(), rb.velocity.z);
-							jumpsLeft -= 1;
-						}
-						jumped = false;
-					}
-					else // Player is on the ground
-					{
-						jumpsLeft = maxJumps;
-						jumped = false;
-						doubleJumpAvailable = true; // Reset double jump availability on landing
-					}
-				}
-
 			}
 		}
 		else
 		{
 			rb.velocity = pushDir * pushForce;
 		}
-		// Apply gravity manually for more tuning control
+		// We apply gravity manually for more tuning control
 		rb.AddForce(new Vector3(0, -gravity * GetComponent<Rigidbody>().mass, 0));
 	}
 
@@ -216,32 +172,6 @@ public class PlayerController : MonoBehaviour {
 		if(other.gameObject.CompareTag("Goal")){
 			GameManager.Instance.PlayerFinished(playerInputHandler.playerConfig.playerIndex);
 		}
-  }
-	public void ActivateDoubleJump()
-	{
-		doubleJumpAvailable = true;
-	}
-
-	public void ActivateExtraLife()
-	{
-		hasExtraLife = true;
-	}
-
-	public void DectivateExtraLife()
-	{
-		hasExtraLife = false;
-	}
-
-	public void ActivateShield()
-	{
-		hasShield = true;
-		this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-	}
-
-	public void DeactivateShield()
-	{
-		hasShield = false;
-		this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
 	}
 
 	float CalculateJumpVerticalSpeed () {
@@ -289,11 +219,5 @@ public class PlayerController : MonoBehaviour {
 			isStunned = false;
 			canMove = true;
 		}
-	}
-
-	public void PlayerDied()
-	{
-		//TODO - add more stuff, e.g. play animation or sfx
-		this.gameObject.SetActive(false);
 	}
 }
